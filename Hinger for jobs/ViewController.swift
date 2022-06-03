038eceee12dd3f9f70ac417489179e6985604521
Maksym Dovhopolyi <maximus200101@gmail.com>
2022-06-03 13:54:03 -0300

    Initial Commit

---
+++ Hinger for jobs/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hinger for jobs
+//
+//  Created by Max Dovhopolyi on 6/3/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
